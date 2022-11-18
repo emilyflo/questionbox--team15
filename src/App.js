a8e29a6cd3c75d39c313eb23664524a9f0cd5a41
Rachel Kelly <racheldk126@gmail.com>
2022-11-18 08:16:58 -0800

    header icons (not functioning as links yet) and basic styling

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import Registration from "./components/Registration.js"
 import Login from "./components/Login";
 import Profile from "./components/Profile";
 import Favorites from "./components/Favorites";
+import Header from './components/Header';
 import Home from "./components/Home";
 import QuestionDetail from "./components/QuestionDetail";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {createTheme} from "@mui/material"
 
 
 function App() {
+
+  // const theme = createTheme({
+  //   spacing: 4,
+  // });
+  
+  // theme.spacing(8); 
+
   return (
     <BrowserRouter>
     {/* <Navbar handleLogout={handleLogout} /> */}
+    <Header/>
       <Routes>
         <Route path="Registration" element={<Registration/>}/>
         <Route path="Login" element={<Login/>}/>
